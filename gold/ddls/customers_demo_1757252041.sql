104be87abc917f1334bef8480a30c9eb2bbcd9eb
mallik-gandhamsetty <57584075+mallik-gandhamsetty@users.noreply.github.com>
2025-09-07 23:34:02 +1000

    feat: Add new demo customer table customers_demo_1757252041

---
+++ gold/ddls/customers_demo_1757252041.sql
@@ -0,0 +1,8 @@
+-n CREATE TABLE IF NOT EXISTS {{catalog}}.gold.customers_demo_1757252041 (
+    id INT,
+    name STRING,
+    email STRING,
+    updated_at TIMESTAMP
+)
+USING DELTA
+COMMENT 'A new demo table created by the interactive demo script.';
